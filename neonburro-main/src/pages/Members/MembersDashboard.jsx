c9c65e9810d2a151c32d0ab949217d18ce21b22b
Tyler Reagan <tyler9reagan@gmail.com>
2025-07-22 02:13:46 -0600

    SHop Updates

--- neonburro-main/src/pages/Members/MembersDashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import InteractiveValleyScene from '../../components/interactive/InteractiveValleyScene';
-
-const MembersDashboard = () => {
-  return <InteractiveValleyScene />;
-};
-
-export default MembersDashboard;
